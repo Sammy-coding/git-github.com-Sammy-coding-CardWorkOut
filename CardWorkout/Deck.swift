0e55d8c2ab94fa39393fe1218c38a4ea971858a2
sammy-coding <samuelodebunmi1@gmail.com>
2023-11-30 14:55:38 +0100

    card workout project

---
+++ CardWorkout/Deck.swift
@@ -0,0 +1,15 @@
+//
+//  Deck.swift
+//  CardWorkout
+//
+//  Created by Mac on 30/11/2023.
+//
+
+import UIKit
+
+struct Decks {
+    static var allValues = [
+        UIImage(named: "addPost")!,
+        UIImage(named: "bp")!
+    ]
+}
